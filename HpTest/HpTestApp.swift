cd61cecac4b68980d9cb51d217176c8286a1fb04
Gabriel Ribeiro <gabrielribeiro@live.com>
2026-05-13 15:09:29 -0300

    Initial Commit

---
+++ HpTest/HpTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  HpTestApp.swift
+//  HpTest
+//
+//  Created by Gabriel Ribeiro on 13/05/26.
+//
+
+import SwiftUI
+
+@main
+struct HpTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
